e7957ca73007aaf3ade10c0d7eef5740c647413d
Kingstar <honeysoft@126.com>
2018-12-30 23:16:26 +0800

    add English comment

--- src/main/java/org/bee/osql/SQL.java
+++ src/main/java/org/bee/osql/SQL.java
@@ -22,36 +22,83 @@ import java.util.List;
 /**
  * 直接用sql语句操作数据库的接口
  * 在该接口中的sql字符串要是DB能识别的SQL语句
+ * The interface of operating database with SQL statement directly
+ * The SQL string in this interface is the SQL statement that DB can recognize
  * @author Kingstar
  * Create on 2013-6-30 下午10:05:36
  * @since  1.0
  */
 public interface SQL {
 	
+	/**
+	 * 根据sql查询数据,数据类型与entity类型一致
+	 * @param sql
+	 * @param entity 与表对应的实体对象,且不能为空
+	 * entity corresponding to table and can not be null.
+	 * @return 返回的list可包含多个实体(多条记录) return list can contain more than one entity
+	 */
 	public <T> List<T> select(String sql,T entity );
 	
+	/**
+	 * 查询部分字段  select some field
+	 * @param sql  select sql string
+	 * @param entity 
+	 * @return
+	 */
 	public <T> List<T> selectSomeField(String sql,T entity );  //调用者:public <T> List<T> select(T entity, String selectField)
 	
+	/**
+	 * SQL function: max,min,avg,sum,count. 如果统计的结果集为空,除了count返回0,其它都返回空字符.
+	 * @param sql
+	 * @return
+	 * @throws ObjSQLException
+	 */
 	public String selectFun(String sql) throws ObjSQLException;
 
 	/**
 	 * @param sql
 	 * @return
 	 * eg:
-	 * select property1,property2 from beanName;
-	 * return list element as: property1[#Bee#]property2 
+	 * select field1,field2 from beanName;
+	 * return list element as: field1[#Bee#]field2 
 	 */
 	public List<String[]> select(String sql);    //PreparedSqlLib ObjSQLRich有调用
 	
+	/**
+	 * 返回json格式结果的查询操作
+	 * @param sql SQL statement to execute
+	 * @return
+	 */
 	public String selectJson(String sql);
 	
 	/**
-	 * 操作update,insert,delete.对应jdbc的executeUpdate方法
-	 * @param sql
+	 * 操作update,insert,delete.对应Jdbc的executeUpdate方法
+	 * operate update,insert,delete. corresponding to Jdbc executeUpdate method.
+	 * @param sql  SQL statement to execute
 	 * @return the number of successful records 返回成功操作的记录行数
 	 */
 	public int modify(String sql);
 	
-	public int[] batch(String sql[]);
-	public int[] batch(String []insertSql,int batchSize);
+	/**
+	 * 提交一批命令到数据库
+	 * Submits a batch of commands to the database.
+	 * @param sqls 要执行的sql语句数组 Array of SQL statements to execute
+	 * @return 包含批中每个命令的一个元素的更新计数所组成的数组
+	 * an array of update counts containing one element for each
+     * command in the batch.  The elements of the array are ordered according
+     * to the order in which commands were added to the batch.
+	 */
+	public int[] batch(String sqls[]);
+	
+	/**
+	 * 提交一批命令到数据库
+	 * Submits a batch of commands to the database.
+	 * @param sqls 要执行的sql语句数组 Array of SQL statements to execute
+	 * @param batchSize
+	 * @return 包含批中每个命令的一个元素的更新计数所组成的数组
+	 * an array of update counts containing one element for each
+     * command in the batch.  The elements of the array are ordered according
+     * to the order in which commands were added to the batch.
+	 */
+	public int[] batch(String []sqls,int batchSize);
 }
