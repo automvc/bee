8959ab54f40f87e1ad2f97a51bd193785550364c
Kingstar <honeysoft@126.com>
2019-11-23 18:25:33 +0800

    v1.6.0 add interface ConditionAssistant for select(T entity, Condition condition)

---
+++ src/main/java/org/teasoft/bee/osql/ConditionAssistant.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2016-2019 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.bee.osql;
+
+import java.util.Set;
+
+/**
+ * @author Kingstar
+ * @since  1.6
+ */
+public interface ConditionAssistant {
+	
+	public Set getFieldSet();
+	
+	 void setSuidType(SuidType SuidType);
+}
