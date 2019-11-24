0e7ad9b26e890818b5f0664722c37f4470a35c30
Kingstar <honeysoft@126.com>
2019-11-25 01:23:01 +0800

    v1.6.0 add between in condition for complicate select.

--- src/main/java/org/teasoft/bee/osql/Condition.java
+++ src/main/java/org/teasoft/bee/osql/Condition.java
@@ -6,18 +6,16 @@
 
 package org.teasoft.bee.osql;
 
-
-
 /**
  * @author Kingstar
  * @since  1.6
  */
-public interface Condition extends ConditionAssistant{
-	
+public interface Condition extends ConditionAssistant {
+
 	public Condition start(Integer start);
+
 	public Condition size(Integer size);
-	
-	
+
 	public Condition op(String field, Op Op, Object value);
 
 	/**
@@ -28,17 +26,47 @@ public interface Condition extends ConditionAssistant{
 	public Condition and();
 
 	public Condition or();
-	
+
 	public Condition lParentheses();
 
 	public Condition rParentheses();
-	
+
+	public Condition between(String field, Number low, Number high);
+	public Condition notBetween(String field, Number low, Number high);
+	public Condition between(String field, String low, String high);
+	public Condition notBetween(String field, String low, String high);
+
 	public Condition groupBy(String field);
 
-	public Condition having(String expression);
+	/**
+	 * eg:having("count(*)>5")  --> having count(*)>5
+	 * @param expressionStr 表达式字符串,field不会被转化.
+	 * expression string,if contain field,it will not be translated.
+	 * @return
+	 */
+	public Condition having(String expressionStr);
+
+	/**
+	 * eg: having(FunctionType.MIN, "field", Op.ge, 60)-->having min(field)>=60
+	 * @param functionType 函数类型
+	 * @param field 实体字段,会被命名转换(如果需要).
+	 * @param Op 操作类型
+	 * @param value 值
+	 * @return
+	 */
+	public Condition having(FunctionType functionType, String field, Op Op, Number value);
 
 	public Condition orderBy(String field);
 
 	public Condition orderBy(String field, OrderType orderType);
+
+	/**
+	 * eg: orderBy(FunctionType.MAX, "total", OrderType.DESC)-->order by max(total) desc
+	 * @param functionType
+	 * @param field
+	 * @param orderType
+	 * @return
+	 */
+	public Condition orderBy(FunctionType functionType, String field, OrderType orderType);
 
 }
