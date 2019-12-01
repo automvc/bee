62444ea6f28d005359ce28937622f690508460d7
Kingstar <honeysoft@126.com>
2019-12-01 23:56:16 +0800

    V1.7.0 more tables select support.

---
+++ src/main/java/org/teasoft/bee/osql/annotation/JoinTable.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2016-2020 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.teasoft.bee.osql.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Kingstar
+ * @since  1.7
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface JoinTable {
+	
+	JoinType joinType() default JoinType.JOIN;  //join type: left join,right join,join (full join)
+//	String mainEntity();
+//	String subEntity();
+	
+	String mainField() default ""; //主表关联的字段
+	String subField() default "";  //从表关联的字段
+	String subAlias() default "";  //从表别名
+	
+	
+
+}
