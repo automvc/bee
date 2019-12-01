c74e6181060d357c043dc6283a1aa0ce222a0753
Kingstar <honeysoft@126.com>
2019-12-01 23:52:51 +0800

    update comment

--- src/main/java/org/teasoft/bee/osql/SuidRich.java
+++ src/main/java/org/teasoft/bee/osql/SuidRich.java
@@ -29,17 +29,31 @@ import java.util.List;
  * @since  1.0
  */
 public interface SuidRich extends Suid {
+	
 	/**
-	 * @param entity 实体类对象 ,且不能为空
+	 * 根据实体对象entity查询数据
+	 * According to entity object select records from database.
+	 * @param entity 实体类对象,且不能为空. table's entity(do not allow null).
+	 * entity中非null且非空字符串作为过虑条件,操作符是等号.eg:field=value
+	 * entity corresponding to table and can not be null.
+	 * If the field value is not null and not empty string as filter condition, 
+	 * the operator is equal sign.eg:field=value
 	 * @param size 结果集大小 大于等于1. fetch result size (>0).
-	 * @return
+	 * @return 返回可包含多个实体(多条记录)的list. return list can contain more than one entity
 	 */
 	public <T> List<T> select(T entity,int size);
+	
 	/**
-	 * @param entity 实体类对象,且不能为空
+	 * 根据实体对象entity查询数据
+	 * According to entity object select records from database.
+	 * @param entity 实体类对象,且不能为空. table's entity(do not allow null).
+	 * entity中非null且非空字符串作为过虑条件,操作符是等号.eg:field=value
+	 * entity corresponding to table and can not be null.
+	 * If the field value is not null and not empty string as filter condition, 
+	 * the operator is equal sign.eg:field=value
 	 * @param start 开始下标(从0或1开始,eg:MySQL是0,Oracle是1).  start index,min value is 0 or 1(eg:MySQL is 0,Oracle is 1).
 	 * @param size 结果集大小 大于等于1. fetch result size (>0).
-	 * @return
+	 * @return 返回可包含多个实体(多条记录)的list. return list can contain more than one entity
 	 */
 	public <T> List<T> select(T entity,int start,int size);
 	
@@ -82,7 +96,7 @@ public interface SuidRich extends Suid {
 	 * select Json type result by entity. 
 	 * @param entity 与表对应的实体对象,且不能为空
 	 * entity中非null与非空字符串作为过虑条件,操作符是等号.eg:field=value
-	 * @return 返回json格式的字符串
+	 * @return 返回可包含多个实体(多条记录)的list转换成的json格式的字符串. return Json string, it transform from list which can contain more than one entity.
 	 * @since  1.1
 	 */
 	public <T> String selectJson(T entity);
@@ -214,7 +228,7 @@ public interface SuidRich extends Suid {
 	 * @param entity 与表对应的实体对象,且不能为空
 	 * id为null不作为过滤条件
 	 * @param includeType 空字符串与null是否包含设置
-	 * @return 返回json格式的字符串
+	 * @return 返回可包含多个实体(多条记录)的list转换成的json格式的字符串. return Json string, it transform from list which can contain more than one entity.
 	 * @since  1.1
 	 */
 	public <T> String selectJson(T entity,IncludeType includeType);
@@ -296,7 +310,8 @@ public interface SuidRich extends Suid {
 	 * @param includeType 空字符串与null是否包含设置
 	 * @param condition 默认有值的字段会转成field=value的形式,其它形式可通过condition指定.condition使用过的字段,默认情况不会再处理.
 	 * if the field is not null or empty, it will be translate to field=value.Other can define with condition. 
-	 * @return 返回json格式的字符串
+	 * 若condition没有设置IncludeType,默认过滤NULL和空字符串(但condition中op,between,notBetween方法设置的字段,不受includeType的值影响.)
+	 * @return 返回可包含多个实体(多条记录)的list转换成的json格式的字符串. return Json string, it transform from list which can contain more than one entity.
 	 * @since  1.6
 	 */
 	public <T> String selectJson(T entity,IncludeType includeType,Condition condition);
