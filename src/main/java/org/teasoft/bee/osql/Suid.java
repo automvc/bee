3b206e5a8ac0da2cc0a576aa09e5f4ce37f77403
Kingstar <honeysoft@126.com>
2020-02-16 23:16:29 +0800

    update comment.

--- src/main/java/org/teasoft/bee/osql/Suid.java
+++ src/main/java/org/teasoft/bee/osql/Suid.java
@@ -21,8 +21,7 @@ import java.util.List;
 
 /**
  * 数据库操作接口,包括查,改,增,删 Suid (select,update,insert,delete),
- * 默认不处理null和空字符串.
- * Database operation: Suid (select,update,insert,delete),
+ * 默认不处理null和空字符串.Database operation: Suid (select,update,insert,delete),
  * the null and empty string are not handled by default.
  * @author Kingstar
  * Create on 2013-6-30 22:03:15
