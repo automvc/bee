4791891c0398c8aae27a608f48f5fd628851ab34
qq202 <2024934252@qq.com>
2020-02-25 20:55:12 +0800

    update comment.

--- src/main/java/org/teasoft/bee/osql/chain/Update.java
+++ src/main/java/org/teasoft/bee/osql/chain/Update.java
@@ -19,6 +19,7 @@ package org.teasoft.bee.osql.chain;
 import org.teasoft.bee.osql.Op;
 
 /**
+ * 链式风格写SQL语句的Update接口.Update interface for SQL with chain programming.
  * @author Kingstar
  * @since  1.3
  */
