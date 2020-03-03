b31b95771e850f94d99529961418abdeb6059343
Kingstar <honeysoft@126.com>
2020-03-03 12:01:25 +0800

    add test/example illustrate.添加测试用例/使用样例.

---
+++ src/test/java/org/teasoft/bee/BeeExam.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2013-2020 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.teasoft.bee;
+
+/**
+ * @author Kingstar
+ * @since  1.7.2
+ */
+public class BeeExam {
+	public static void main(String[] args) {
+//		测试用例请查看工程:bee-exam
+//		以下任意一个地址：
+//		https://gitee.com/automvc/bee-exam
+//		https://github.com/automvc/bee-exam
+//
+//		让开发爱上测试，从Bee开始！
+		
+		System.out.println("测试用例请查看工程:bee-exam");
+		System.out.println("以下任意一个地址：");
+		System.out.println("https://gitee.com/automvc/bee-exam");
+		System.out.println("https://github.com/automvc/bee-exam");
+		System.out.println("");
+		System.out.println("让开发爱上测试，从Bee开始！");
+		
+//		test case see new project:bee-exam
+//		url as below：
+//		https://github.com/automvc/bee-exam
+//		https://gitee.com/automvc/bee-exam
+//
+//		Let developers like test, starting with Bee!
+
+	}
+}
