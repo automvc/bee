e175a915d41549bab3826408136fa11301a3730a
Kingstar <honeysoft@126.com>
2020-04-17 18:36:23 +0800

    v1.7.2 add BeeIllegalBusinessException

---
+++ src/main/java/org/teasoft/bee/osql/exception/BeeIllegalBusinessException.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2016-2020 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.teasoft.bee.osql.exception;
+
+import org.teasoft.bee.osql.BeeException;
+
+/**
+ * 非法业务异常.Illegal business Exception.
+ * @author Kingstar
+ * @since  1.7.2
+ */
+public class BeeIllegalBusinessException extends BeeException {
+	
+	static final long serialVersionUID = -875516993124222230L;
+
+
+	public BeeIllegalBusinessException() {
+		super();
+	}
+
+	public BeeIllegalBusinessException(String message) {
+		super(message);
+	}
+
+	public BeeIllegalBusinessException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public BeeIllegalBusinessException(Throwable cause) {
+		super(cause);
+	}
+
+}
