c400019ece45987ba99c64475b39c326fd72e891
Kingstar <honeysoft@126.com>
2020-05-23 21:09:47 +0800

    enhance cn & en comment.

--- src/main/java/org/teasoft/bee/osql/MoreTable.java
+++ src/main/java/org/teasoft/bee/osql/MoreTable.java
@@ -81,10 +81,10 @@ public interface MoreTable {
 	/**
 	 * 根据实体对象entity查询数据
 	 * According to entity object select records from database.
-	 * @param entity 与表对应的实体对象,且不能为空. table's entity(do not allow null).
-	 * entity中非null且非空字符串作为过虑条件,操作符是等号.eg:field=value
-	 * entity corresponding to table and can not be null.
-	 * If the field value is not null and not empty string as filter condition, 
+	 * @param entity 与表对应的实体对象,且不能为空. table's entity(do not allow null).<br>
+	 * entity中非null且非空字符串作为过虑条件,操作符是等号.eg:field=value<br>
+	 * entity corresponding to table and can not be null.<br>
+	 * If the field value is not null and not empty string as filter condition, <br>
 	 * the operator is equal sign.eg:field=value
 	 * @return 返回可包含多个实体(多条记录)的list. return list can contain more than one entity
 	 */
@@ -93,10 +93,10 @@ public interface MoreTable {
 	/**
 	 * 根据实体对象entity查询数据
 	 * According to entity object select records from database.
-	 * @param entity 实体类对象,且不能为空. table's entity(do not allow null).
-	 * entity中非null且非空字符串作为过虑条件,操作符是等号.eg:field=value
-	 * entity corresponding to table and can not be null.
-	 * If the field value is not null and not empty string as filter condition, 
+	 * @param entity 实体类对象,且不能为空. table's entity(do not allow null).<br>
+	 * entity中非null且非空字符串作为过虑条件,操作符是等号.eg:field=value<br>
+	 * entity corresponding to table and can not be null.<br>
+	 * If the field value is not null and not empty string as filter condition, <br>
 	 * the operator is equal sign.eg:field=value
 	 * @param start 开始下标(从0或1开始,eg:MySQL是0,Oracle是1).  start index,min value is 0 or 1(eg:MySQL is 0,Oracle is 1).
 	 * @param size 结果集大小 大于等于1. fetch result size (>0).
@@ -106,9 +106,9 @@ public interface MoreTable {
 
 	/**
 	 * 根据实体对象和Condition查询数据.Select the records according to entity and condition.
-	 * @param entity 与表对应的实体对象,且不能为空. table's entity(do not allow null).
-	 * @param condition 默认有值的字段会转成field=value的形式,其它形式可通过condition指定.condition使用过的字段,默认情况不会再处理.
-	 * if the field is not null or empty, it will be translate to field=value.Other can define with condition. 
+	 * @param entity 与表对应的实体对象,且不能为空. table's entity(do not allow null).<br>
+	 * @param condition entity默认有值的字段会转成field=value的形式,其它形式可通过condition指定.condition使用过的字段,默认情况不会再处理.<br>
+	 * If the field of entity is not null or empty, it will be translate to field=value.Other can define with condition. <br>
 	 * 若condition没有设置IncludeType,默认过滤NULL和空字符串(但condition中op,between,notBetween方法设置的字段,不受includeType的值影响.)
 	 * @return 返回可包含多个实体(多条记录)的list. return list can contain more than one entity
 	 */
