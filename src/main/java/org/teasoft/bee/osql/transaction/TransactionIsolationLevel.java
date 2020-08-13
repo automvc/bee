a1c3a8848bc55a0964185fcef1435ade73696a67
teasoft <teasoft@163.com>
2020-08-13 21:25:45 +0800

    update src/main/java/org/teasoft/bee/osql/transaction/TransactionIsolationLevel.java.

--- src/main/java/org/teasoft/bee/osql/transaction/TransactionIsolationLevel.java
+++ src/main/java/org/teasoft/bee/osql/transaction/TransactionIsolationLevel.java
@@ -24,11 +24,51 @@ package org.teasoft.bee.osql.transaction;
  */
 public enum TransactionIsolationLevel
 {
-    TRANSACTION_NONE(0), 
+    /**
+     * 指示事务不受支持的常量.A constant indicating that transactions are not supported.
+     */
+	TRANSACTION_NONE(0), 
+	/**
+	 * 指示可以发生:脏读 (dirty read)、不可重复读和虚读 (phantom read) 的常量<br>
+	 * A constant indicating that dirty reads, non-repeatable reads and phantom reads can occur.
+     * This level allows a row changed by one transaction to be read
+     * by another transaction before any changes in that row have been
+     * committed (a "dirty read").  If any of the changes are rolled back,
+     * the second transaction will have retrieved an invalid row.
+	 */
     TRANSACTION_READ_UNCOMMITTED(1), 
+    
+    /**
+     * 指示不可以发生脏读的常量；不可重复读和虚读可以发生<br>A constant indicating that
+     * dirty reads are prevented; non-repeatable reads and phantom reads can occur.
+     * This level only prohibits a transaction
+     * from reading a row with uncommitted changes in it.
+     */
     TRANSACTION_READ_COMMITTED(2), 
 
+    /**
+     * 指示不可以发生脏读和不可重复读的常量；虚读可以发生<br>
+     * A constant indicating that dirty reads and non-repeatable reads are prevented; phantom reads can occur.  
+     * This level prohibits a transaction from
+     * reading a row with uncommitted changes in it, and it also
+     * prohibits the situation where one transaction reads a row,
+     * a second transaction alters the row, and the first transaction
+     * rereads the row, getting different values the second time
+     * (a "non-repeatable read").
+     */
     TRANSACTION_REPEATABLE_READ(4), 
+    
+    /**
+     * 指示不可以发生脏读、不可重复读和虚读的常量<br>A constant indicating that
+     * dirty reads, non-repeatable reads and phantom reads are prevented.
+     * This level includes the prohibitions in
+     * <code>TRANSACTION_REPEATABLE_READ</code> and further prohibits the
+     * situation where one transaction reads all rows that satisfy
+     * a <code>WHERE</code> condition, a second transaction inserts a row that
+     * satisfies that <code>WHERE</code> condition, and the first transaction
+     * rereads for the same condition, retrieving the additional
+     * "phantom" row in the second read.
+     */
     TRANSACTION_SERIALIZABLE(8);
 
 	private final int level;
@@ -37,6 +77,10 @@ public enum TransactionIsolationLevel
 		this.level = level;
 	}
 
+	/**
+	 * 获取隔离级别.get the value of TransactionIsolationLevel.
+	 * @return 隔离级别的值.value of TransactionIsolationLevel.
+	 */
 	public int getLevel() {
 		return this.level;
 	}
