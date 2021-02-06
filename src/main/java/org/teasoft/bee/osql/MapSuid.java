d7f31cc35e528ec5176f7b66ccdb668d118e560f
Kingstar <honeysoft@126.com>
2021-02-07 00:14:10 +0800

    V1.9 Database operation interface using map instead of specific entity.

---
+++ src/main/java/org/teasoft/bee/osql/MapSuid.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2016-2021 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.teasoft.bee.osql;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 使用Map而非具体实体的数据库操作接口,包括查,改,增,删 Suid (select,update,insert,delete),
+ * 默认不处理null和空字符串
+ * <br>Database operation interface using map instead of specific entity. 
+ * <br>Suid (select,update,insert,delete),
+ * the null and empty string are not handled by default.
+ * @author Kingstar
+ * @since  1.9
+ */
+public interface MapSuid {
+
+	/**
+	 * 使用MapSql查询数据.Use MapSql to select data.
+	 * @param mapSql MapSql instance
+	 * @return List<String[]>结构的多行记录.
+	 * <br>the multi-line record of List<String[]> structure.
+	 */
+	public List<String[]> selectString(MapSql mapSql);
+
+	/**
+	 * 使用MapSql查询数据.Use MapSql to select data.
+	 * @param mapSql MapSql instance
+	 * @return Json格式的多行记录.multi-line record in JSON format.
+	 */
+	public String selectJson(MapSql mapSql);
+
+	/**
+	 * 使用MapSql查询数据.Use MapSql to select data.
+	 * @param mapSql MapSql instance
+	 * @return List<Map<String, Object>>结构的多行记录.
+	 * <br>the multi-line record of List<Map<String, Object>> structure.
+	 */
+	public List<Map<String, Object>> select(MapSql mapSql);
+
+	/**
+	 * 使用MapSql查询数据.Use MapSql to select data.
+	 * @param mapSql MapSql instance
+	 * @return 包装在一个Map中的一行数据.returns a row of data wrapped in a map.
+	 */
+	public Map<String, Object> selectOne(MapSql mapSql);
+
+	/**
+	 * 使用MapSql查询数据.Use MapSql to select data.
+	 * @param mapSql MapSql instance
+	 * @return 成功删除的记录数. the numbers of delete records successfully. 
+	 */
+	public int delete(MapSql mapSql);
+
+}
