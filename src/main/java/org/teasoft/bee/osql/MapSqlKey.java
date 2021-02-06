fd5fa435bb71690f158b4e0ea70588b73f75f115
Kingstar <honeysoft@126.com>
2021-02-07 00:13:56 +0800

    V1.9 Database operation interface using map instead of specific entity.

---
+++ src/main/java/org/teasoft/bee/osql/MapSqlKey.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2016-2021 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.teasoft.bee.osql;
+
+/**
+ * @author Kingstar
+ * @since  1.9
+ */
+public enum MapSqlKey {
+	
+    Table("Table"),
+    SelectColumns("SelectColumns"),
+    GroupBy("GroupBy"),
+    Having("Having"),
+    OrderBy("OrderBy"),
+    
+//	Start("Start"),
+//	Size("Size"),
+    
+    /**
+     * 是否命令转换.Is naming transfer. 'true' or 'false',default is 'false'
+     * 真时,会根据配置的规则(bee.properties)如进行转换;假时不转换.
+     * if true, will transfer by the rule (eg: bee.properties). if false,do not transfer.
+     */
+    IsNamingTransfer("IsNamingTransfer") ;
+	
+    String name;
+    
+    MapSqlKey(String name){
+    	this.name = name;  
+    }
+    
+    public String getName(){
+    	return name;
+    }
+
+}
