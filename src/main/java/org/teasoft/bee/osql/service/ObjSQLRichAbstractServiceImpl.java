4eb0aeda5d9a57455cde114704453708c0c98753
Kingstar <honeysoft@126.com>
2021-03-23 10:41:32 +0800

    add method setDynamicParameter in SuidRich for coding programming

--- src/main/java/org/teasoft/bee/osql/service/ObjSQLRichAbstractServiceImpl.java
+++ src/main/java/org/teasoft/bee/osql/service/ObjSQLRichAbstractServiceImpl.java
@@ -23,6 +23,7 @@ import org.teasoft.bee.osql.Condition;
 import org.teasoft.bee.osql.FunctionType;
 import org.teasoft.bee.osql.IncludeType;
 import org.teasoft.bee.osql.OrderType;
+import org.teasoft.bee.osql.Suid;
 import org.teasoft.bee.osql.SuidRich;
 
 /**
@@ -312,4 +313,9 @@ public abstract class ObjSQLRichAbstractServiceImpl extends ObjSQLAbstractServic
 		return getSuidRich().selectString(entity, condition);
 	}
 	
+	@Override
+	public SuidRich setDynamicParameter(String para, String value) {
+		//Add the business logic if need.
+		return getSuidRich().setDynamicParameter(para, value);
+	}
 }
