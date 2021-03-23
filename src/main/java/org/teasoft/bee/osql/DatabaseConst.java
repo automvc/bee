b83b2eaf8041278b755055aafb4da80822a90a92
Kingstar <honeysoft@126.com>
2021-03-23 23:14:51 +0800

    update DatabaseConst

--- src/main/java/org/teasoft/bee/osql/DatabaseConst.java
+++ src/main/java/org/teasoft/bee/osql/DatabaseConst.java
@@ -10,20 +10,21 @@ public interface DatabaseConst {
 	String MYSQL = "MySQL";
 	String MariaDB = "MariaDB";
 	String ORACLE = "Oracle";
-	String SQLSERVER = "sqlserver";
+	String SQLSERVER = "Microsoft SQL Server";
 
 	String H2 = "H2";
 	String SQLite = "SQLite";
 	String PostgreSQL = "PostgreSQL";
 
 	String Cubrid = "Cubrid";
-	String DB2 = "DB2";
-	String Derby = "Derby";
+	String DB2400 = "DB2 UDB for AS/400";
+	String DB2 = "DB2";  //todo
+	String Derby = "Apache Derby";
 	String Firebird = "Firebird";
 	String FrontBase = "FrontBase";
 
-	String HSQL = "HSQL";
-	String Informix = "Informix";
+	String HSQL = "HSQL Database Engine";
+	String Informix = "Informix Dynamic Server";
 	String Ingres = "Ingres";
 	String JDataStore = "JDataStore";
 	String Mckoi = "Mckoi";
@@ -31,7 +32,7 @@ public interface DatabaseConst {
 	String Pointbase = "Pointbase";
 
 	String SAPDB = "SAPDB";
-	String Sybase = "Sybase";
+	String Sybase = "Sybase SQL Server";
 	String Teradata = "Teradata";
 	String TimesTen = "TimesTen";
 
