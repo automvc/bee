922700f37c3224654e7e0d6aa293e776f5d31730
Kingstar <honeysoft@126.com>
2021-03-25 13:59:19 +0800

    test V1.9 with well-known and major database

--- src/test/java/org/teasoft/bee/BeeExam.java
+++ src/test/java/org/teasoft/bee/BeeExam.java
@@ -28,6 +28,60 @@ public class BeeExam {
 	@Test
 	public void test(){
 		
+//		测试用例请查看工程:bee-exam 
+//		以下任意一个地址：
+//		https://gitee.com/automvc/bee-exam
+//		https://github.com/automvc/bee-exam
+//
+//		让开发爱上测试，从Bee开始！
+		
+		boolean isFinished=false;
+		
+		System.out.println("测试用例请查看工程:bee-exam");
+		System.out.println("以下任意一个地址：");
+		System.out.println("https://gitee.com/automvc/bee-exam");
+		System.out.println("https://github.com/automvc/bee-exam");
+		System.out.println("");
+		System.out.println("让开发爱上测试，从Bee开始！");
+		System.out.println("");
+		
+		System.out.println("------------------------");
+		System.out.println("");
+//		test case see new project:bee-exam
+//		url as below：
+//		https://github.com/automvc/bee-exam
+//		https://gitee.com/automvc/bee-exam
+//
+//		Let developers like test, starting with Bee!
+		
+		System.out.println("test case see new project:bee-exam");
+		System.out.println("url as below：");
+		System.out.println("https://github.com/automvc/bee-exam");
+		System.out.println("https://gitee.com/automvc/bee-exam");
+		System.out.println("");
+		System.out.println("Let developers like test, starting with Bee!");
+		
+		isFinished=true;
+		Assert.assertEquals(isFinished,true);
+		
+		
+//		2021-03-20 (yyyy-MM-dd)
+//		Instructions Counters	
+//		Element	   Coverage
+//		Bee	        91.6%
+//		Honey	    65.6%
+//			
+//		Method Counters	
+//		Element	  Coverage
+//		Bee	       88.0%
+//		Honey	   70.3%
+//			
+//		Type Counters	
+//		Element	  Coverage
+//		Bee	       97.0%
+//		Honey	   86.3%
+		
+		
 //		bee-exam last update time:2020-06-13 13:14 (yyyy-MM-dd HH:mm)
 	  //add multi-DataSource support(Write/Read, only Split Database)
 //		bee-exam last update time:2020-06-27 16:40 (yyyy-MM-dd HH:mm) 
@@ -70,58 +124,15 @@ public class BeeExam {
 //		bee-exam(v1.9)-cache(Oracle)-2021-03-20 20.02.02.758.txt
 //		bee-exam(v1.9)-normal(MySQL)-2021-03-20 23.10.06.324.txt
 //		bee-exam(v1.9)-cache(MySQL)-2021-03-20 23.10.16.346.txt
+//		bee-exam(v1.9)-normal(SQLite)-2021-03-24 22.10.08.975.txt
 		
-//		2021-03-20 (yyyy-MM-dd)
-//		Instructions Counters	
-//		Element	   Coverage
-//		Bee	        91.6%
-//		Honey	    65.6%
-//			
-//		Method Counters	
-//		Element	  Coverage
-//		Bee	       88.0%
-//		Honey	   70.3%
-//			
-//		Type Counters	
-//		Element	  Coverage
-//		Bee	       97.0%
-//		Honey	   86.3%
-		
-//		测试用例请查看工程:bee-exam 
-//		以下任意一个地址：
-//		https://gitee.com/automvc/bee-exam
-//		https://github.com/automvc/bee-exam
-//
-//		让开发爱上测试，从Bee开始！
-		
-		boolean isFinished=false;
-		
-		System.out.println("测试用例请查看工程:bee-exam");
-		System.out.println("以下任意一个地址：");
-		System.out.println("https://gitee.com/automvc/bee-exam");
-		System.out.println("https://github.com/automvc/bee-exam");
-		System.out.println("");
-		System.out.println("让开发爱上测试，从Bee开始！");
-		System.out.println("");
-		
-		System.out.println("------------------------");
-		System.out.println("");
-//		test case see new project:bee-exam
-//		url as below：
-//		https://github.com/automvc/bee-exam
-//		https://gitee.com/automvc/bee-exam
-//
-//		Let developers like test, starting with Bee!
-		
-		System.out.println("test case see new project:bee-exam");
-		System.out.println("url as below：");
-		System.out.println("https://github.com/automvc/bee-exam");
-		System.out.println("https://gitee.com/automvc/bee-exam");
-		System.out.println("");
-		System.out.println("Let developers like test, starting with Bee!");
-		
-		isFinished=true;
-		Assert.assertEquals(isFinished,true);
+//      test V1.9 with well-known and major database 
+//		bee-exam(v1.9)2021-03-25 13.10.37.514-normal(Microsoft SQL Server).txt
+//		bee-exam(v1.9)2021-03-25 13.14.57.616-normal(SQLite).txt
+//		bee-exam(v1.9)2021-03-25 13.17.51.312-normal(MySQL).txt
+//		bee-exam(v1.9)2021-03-25 13.20.15.761-normal(Oracle).txt
+//		bee-exam(v1.9)2021-03-25 13.23.08.180-normal(H2).txt
+//		bee-exam(v1.9)2021-03-25 13.25.51.23-normal(PostgreSQL).txt
 
 	}
 }
