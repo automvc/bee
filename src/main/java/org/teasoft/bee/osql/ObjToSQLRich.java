d67980818cbfdf90986bf58bac6fe020a34213d4
Kingstar <honeysoft@126.com>
2021-04-08 18:57:40 +0800

    close one method

--- src/main/java/org/teasoft/bee/osql/ObjToSQLRich.java
+++ src/main/java/org/teasoft/bee/osql/ObjToSQLRich.java
@@ -46,7 +46,7 @@ public interface ObjToSQLRich extends ObjToSQL {
 	public  <T> String toUpdateSQL(T entity, IncludeType includeType);
 	
 	public  <T> String[] toInsertSQL(T[] entity);
-	public  <T> String[] toInsertSQL(T[] entity,int batchSize);
+//	public  <T> String[] toInsertSQL(T[] entity,int batchSize);
 	public  <T> String[] toInsertSQL(T[] entity,String excludeFields);
 	public  <T> String[] toInsertSQL(T[] entity,int batchSize,String excludeFields);
 	
