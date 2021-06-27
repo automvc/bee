aa39ae2696ac54055f4ae9c9201046a2b9a91cda
Kingstar <honeysoft@126.com>
2021-06-28 00:06:24 +0800

    add BeeErrorNameException

---
+++ src/main/java/org/teasoft/bee/osql/exception/BeeErrorNameException.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2016-2020 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.teasoft.bee.osql.exception;
+
+import org.teasoft.bee.osql.ObjSQLException;
+
+/**
+ * 变量名命名异常.Bee error name exception.
+ * @author Kingstar
+ * @since  1.4
+ */
+public class BeeErrorNameException extends ObjSQLException {
+	static final long serialVersionUID = -875516993124222236L;
+
+
+	public BeeErrorNameException() {
+		super();
+	}
+
+	public BeeErrorNameException(String message) {
+		super(message);
+	}
+
+	public BeeErrorNameException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public BeeErrorNameException(Throwable cause) {
+		super(cause);
+	}
+}
