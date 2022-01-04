4c009452f2d575663addc0a5f809fcd03b71d291
Kingstar <honeysoft@126.com>
2022-01-04 23:24:13 +0800

    update PrimaryKey

--- src/main/java/org/teasoft/bee/osql/annotation/PrimaryKey.java
+++ src/main/java/org/teasoft/bee/osql/annotation/PrimaryKey.java
@@ -12,12 +12,17 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 标注某个属性为主键
+ * 标注某个属性为主键<br>
+ * 不允许定义一个字段名为id,但却不将它定义为主键.<br>
+ * 当主键的字段名不是id时,可用PrimaryKey注解标注为主键.<br>
+ * 当字段有Ignore注解时,PrimaryKey注解也会被忽略.<br>
+ * 此注解是为兼容旧系统而存在,<br>
+ * 强烈建议,新系统定义表时设置一个id列为主键
  * @author Kingstar
  * @since  1.11
  */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PrimaryKey {
-	String value() default ""; 
+	
 }
