00c762cdf6c740c0509948f9329e14fd887cae44
Kingstar <honeysoft@126.com>
2022-01-30 23:22:48 +0800

    add Properties interface

---
+++ src/main/java/org/teasoft/bee/osql/Properties.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2016-2021 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.bee.osql;
+
+/**
+ * @author Kingstar
+ * @since  1.11
+ */
+public interface Properties {
+
+	public String getProp(String key);
+
+	public String getPropText(String key);
+
+}
