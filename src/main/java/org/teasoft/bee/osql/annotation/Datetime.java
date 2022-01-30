8d3ff559ad4f7591ae1946fd3ef79f390f4dac84
Kingstar <honeysoft@126.com>
2022-01-30 23:23:59 +0800

    add Datetime annotation

---
+++ src/main/java/org/teasoft/bee/osql/annotation/Datetime.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2016-2021 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.bee.osql.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.teasoft.bee.osql.SuidType;
+
+/**
+ * @author Kingstar
+ * @since  1.11
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Datetime {
+
+	String value() default "";
+
+	String formatter();
+
+	String override();
+	
+	SuidType suidType() default SuidType.INSERT;
+
+}
