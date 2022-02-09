92b70f1ee8676b4461e2f40ff8d73355990ecfe0
Kingstar <honeysoft@126.com>
2022-02-09 21:07:51 +0800

    update BeeExtCache

--- src/main/java/org/teasoft/bee/osql/BeeExtCache.java
+++ src/main/java/org/teasoft/bee/osql/BeeExtCache.java
@@ -18,28 +18,32 @@
 package org.teasoft.bee.osql;
 
 /**
+ * 扩展的缓存接口.Ext Cache interface.
  * @author Kingstar
  * @since  1.11
  */
 public interface BeeExtCache extends Cache{
 
 	/**
-	 * 
-	 * @param key
-	 * @return
+	 * 通过key获取缓存结果记录,key一般是查询用的sql
+	 * Get the cache result by key(sql). 
+	 * @param key cache key,normally it is sql.
+	 * @return 结果记录
 	 */
 	public Object getInExtCache(String key);
 
 	/**
-	 * 
-	 * @param key
-	 * @param result
+	 * 将结果集放入缓存
+	 * Put the result in the cache identify with key(sql).
+	 * @param key  cache key,normally it is sql.
+	 * @param result 结果记录
 	 */
 	public void addInExtCache(String key, Object result);
 
 	/**
-	 * 
-	 * @param key
+	 * 清除某个表相关的缓存
+	 * Clear the cache result of related table by key(sql).
+	 * @param key cache key,normally it is sql.
 	 */
 	public void clearInExtCache(String key);
 
