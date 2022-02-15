704acf363d19db72aaa2c814c65880a309c07918
Kingstar <honeysoft@126.com>
2022-02-15 20:58:40 +0800

    add:AnnotationHandler,AutoSetString

---
+++ src/main/java/org/teasoft/bee/osql/annotation/AnnotationHandler.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2016-2022 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.bee.osql.annotation;
+
+/**
+ * 注解处理器接口
+ * Annotation Handler Interface.
+ * @author Kingstar
+ * @since  1.11
+ */
+public interface AnnotationHandler {
+
+	public String process();
+
+}
