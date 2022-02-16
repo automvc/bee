d5a23e813fc72f674ad41a09475844feccf8e437
Kingstar <honeysoft@126.com>
2022-02-16 20:13:03 +0800

    add BeeVersion

---
+++ src/main/java/org/teasoft/bee/osql/BeeVersion.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2016-2022 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.bee.osql;
+
+/**
+ * @author Kingstar
+ * @since  1.11
+ */
+final public class BeeVersion {
+
+	public static final String version = "V1.11";
+	public static final String buildId = "V1.11.0.2.20";
+
+}
