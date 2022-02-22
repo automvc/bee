2a28252931de2bfe289af3c3ac0923b20139ef87
Kingstar <honeysoft@126.com>
2022-02-22 10:32:51 +0800

    add Calculate

---
+++ src/main/java/org/teasoft/bee/distribution/sharding/algorithm/Calculate.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2016-2022 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.teasoft.bee.distribution.sharding.algorithm;
+
+/**
+ * @author Kingstar
+ * @since  1.11
+ */
+public interface Calculate {
+	
+//	Long process(Long id);
+//
+//	String process(String str);
+	
+	String process(String rule, String shardingValue);
+}
