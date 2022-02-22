35cae629b2e3ccba659e40806ad10afdefefa3dd
Kingstar <honeysoft@126.com>
2022-02-22 10:30:37 +0800

    update MultiTenancy

--- src/main/java/org/teasoft/bee/osql/annotation/customizable/MultiTenancy.java
+++ src/main/java/org/teasoft/bee/osql/annotation/customizable/MultiTenancy.java
@@ -34,33 +34,48 @@ import java.lang.annotation.Target;
 public @interface MultiTenancy {
 	
 //Order: 1->2
-//	1.特指 (配置了特批的,其它不再检测)
+//	1.特指 (配置了特指的,其它不再检测)
 //	2.通过算法计算(若没有匹配的则用默认)
 	
 	String appointDS() default "";
+	
+	String appointTab() default "";
 	
 	/**
 	 * 0:求余等能直接计算的运算, 1:自定义,  2:范围???  3:日期等取部分字符串??
 	 * @return
 	 */
-    int dbAlgorithm() default 0; 
+    int dsAlgorithm() default 0; 
     int tabAlgorithm() default 0; 
     
 	
 	/**
-	 * eg: "id%dbSize
+	 * dsRule不设置,即使用默认的空值,则表示,不用管库名
+	 * eg: "id%dsSize
 	 */
-	String dbRule() default "";
-	String dbName() default "db${dbRule}";
+	String dsRule() default "";
+	
+	/**
+	 * dsRule为空,也没设置dsName的值,则不需要带库名
+	 * 默认是dsRule()计算得的值自动作为dsName的后缀.
+	 * 
+	 * @return
+	 */
+	String dsName() default "";
 	
 	
 	String tabRule() default "";
-	String tabName() default "tab${tabRule}";
+	/**
+	 * 默认是tabRule()计算得的值作为tabName的后缀.
+	 * eg:tab${tabRule}
+	 * @return
+	 */
+	String tabName() default "";
 	
 	/**
 	 * 要以定义处理器
 	 * @return
 	 */
-	Class<? extends AnnotationHandler> handler() default AnnotationHandler.class;
+	Class<? extends DsTabHandler> handler() default DsTabHandler.class;
 
 }
