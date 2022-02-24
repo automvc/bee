2a132b65bf952593e347a1c82b7f81c20d6ba04b
Kingstar <honeysoft@126.com>
2022-02-24 22:10:53 +0800

    update the comment

---
+++ src/main/java/org/teasoft/bee/osql/annotation/customizable/ColumnHandler.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2020-2022 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.teasoft.bee.osql.annotation.customizable;
+
+/**
+ * Column注解处理器接口
+ * Column Annotation handler interface
+ * @author Kingstar
+ * @since  1.11
+ */
+@SuppressWarnings("rawtypes")
+public interface ColumnHandler {
+	
+	public String toColumnName(String fieldName, Class entityClass);
+
+	public String toFieldName(String columnName, Class entityClass);
+	
+}
