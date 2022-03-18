a65e6fc6eae9ab347132b6b5e711f34b0c00c976
Kingstar <honeysoft@126.com>
2022-03-18 17:58:10 +0800

    add comment for CustomAutoSqlToken

--- src/main/java/org/teasoft/bee/osql/token/CustomAutoSqlToken.java
+++ src/main/java/org/teasoft/bee/osql/token/CustomAutoSqlToken.java
@@ -9,12 +9,26 @@ package org.teasoft.bee.osql.token;
 /**
  * 自定义动态SQL标签
  * 不能用在批量插入操作.
+ * Custom auto SQL token.
+ * do not effect in batch insert.
  * @author Kingstar
  * @since  1.11
  */
 public final class CustomAutoSqlToken {
+	
+	/**
+	 * the para is not null,will transfer.
+	 * eg:
+	 * <if isNotNull>userid in #{userid@in}</if>
+	 * if value of userid is not null, will parse "userid in #{userid@in}"
+	 */
 	public static final String isNotNull = "<if isNotNull>";
+	
+	/**
+	 * if value of para is not null and not empty string,will transfer.
+	 */
 	public static final String isNotBlank = "<if isNotBlank>";
+	
 	public static final String endIf = "</if>";
 	
 	/**
@@ -24,11 +38,18 @@ public final class CustomAutoSqlToken {
 	 */
 	public static final String atIn="@in";
 	
-	//when the value is null, name=#{name@toIsNULL1} -> name is null (delete one char '='
+	/**
+	 * when the value is null, name=#{name@toIsNULL1} -> name is null (delete one char '='
+	 */
 	public static final String toIsNULL1="@toIsNULL1";
-	//when the value is null, num>=#{num@toIsNULL2} -> name is null (delete two char '>=')
+	
+	/**
+	 * when the value is null, num>=#{num@toIsNULL2} -> name is null (delete two char '>=')
+	 */
 	public static final String toIsNULL2="@toIsNULL2";
 	
-	//just use in like
+	/**
+	 * just use in like
+	 */
 	public static final String percent ="%";
 }
