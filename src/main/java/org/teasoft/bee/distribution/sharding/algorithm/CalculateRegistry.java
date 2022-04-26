0e445b3a1fe8c95f5f617d7d8a5444b1e6152dac
Kingstar <honeysoft@126.com>
2022-04-27 00:49:44 +0800

    add CalculateRegistry

---
+++ src/main/java/org/teasoft/bee/distribution/sharding/algorithm/CalculateRegistry.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2016-2022 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.bee.distribution.sharding.algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.teasoft.bee.osql.Registry;
+
+/**
+ * Calculate Registry
+ * @author Kingstar
+ * @since  1.11
+ */
+public class CalculateRegistry implements Registry {
+
+	private static final Map<String, Calculate> calculateMap = new HashMap<>();
+
+	/**
+	 * register Calculate.
+	 * @param type number type
+	 * @param calculate instance of Calculate type
+	 */
+	public static void register(int type, Calculate calculate) {
+		calculateMap.put(type + "", calculate);
+	}
+
+	/**
+	 * get Calculate.
+	 * @param type number type
+	 * @return instance of Calculate type
+	 */
+	public static Calculate getCalculate(int type) {
+		return calculateMap.get(type + "");
+	}
+
+}
