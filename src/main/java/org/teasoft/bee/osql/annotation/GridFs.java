16e8697fde3a9929be7d7b40512a03c28d4cd803
Kingstar <honeysoft@126.com>
2023-03-12 12:28:30 +0800

    update comment

--- src/main/java/org/teasoft/bee/osql/annotation/GridFs.java
+++ src/main/java/org/teasoft/bee/osql/annotation/GridFs.java
@@ -1,7 +1,18 @@
 /*
- * Copyright 2016-2023 the original author.All rights reserved.
+ * Copyright 2020-2023 the original author.All rights reserved.
  * Kingstar(honeysoft@126.com)
- * The license,see the LICENSE file.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.teasoft.bee.osql.annotation;
@@ -12,16 +23,19 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * store the file for Mongodb and register the file id to the field which define in fileIdName.
+ * Store the file for Mongodb and register the file id to the field which define in fileIdName.
+ * The filename from the field which define in fileName.
+ * The file type is InputStream.
  * @author Kingstar
  * @since  2.1
  */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface GridFs {
-	
-	//用于Mongodb声明, 保存文件到数据库后,会将文件id保存到fileIdName声明的字段(默认是:fileid).
+
+	// 用于Mongodb声明, 保存文件到数据库后,会将文件id保存到fileIdName声明的字段(默认是:fileid).
 	String fileIdName() default "fileid";
+
 //	文件名对应的字段名
 	String fileName() default "filename";
 }
