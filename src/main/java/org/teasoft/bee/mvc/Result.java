24f04287df4cafcc98f989988fc6be1435f78702
Kingstar <honeysoft@126.com>
2023-05-13 23:23:48 +0800

    add Result struct for MVC develope

---
+++ src/main/java/org/teasoft/bee/mvc/Result.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2020-2023 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.teasoft.bee.mvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Kingstar
+ * @since  2.1
+ */
+public class Result {
+
+	private int total;
+	private List<?> rows;
+	private String code;
+	private String msg;
+	private String errorMsg;
+
+	public Result() {
+		rows = new ArrayList<>(1);
+	}
+
+	public Result(String errorMsg) {
+		this.errorMsg = errorMsg;
+		rows = new ArrayList<>(1); // prevent front end null exception; 预防前端页面null异常
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+	public List<?> getRows() {
+		return rows;
+	}
+
+	public void setRows(List<?> rows) {
+		this.rows = rows;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public String getErrorMsg() {
+		return errorMsg;
+	}
+
+	public void setErrorMsg(String errorMsg) {
+		this.errorMsg = errorMsg;
+	}
+
+}
