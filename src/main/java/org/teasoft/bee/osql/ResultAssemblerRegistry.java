c3e7bfbe0bede303d3da80cde4c5dd676dc2ed6d
Kingstar <honeysoft@126.com>
2024-05-26 23:56:49 +0800

    add select Result Assembler

---
+++ src/main/java/org/teasoft/bee/osql/ResultAssemblerRegistry.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022-2024 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.teasoft.bee.osql;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Kingstar
+ * @since  2.4.0
+ */
+public class ResultAssemblerRegistry implements Registry {
+
+	private static final Map<Class<?>, ResultAssembler<?>> assemblerMap = new HashMap<>();
+
+	public static <T> void register(Class<T> c, ResultAssembler<T> assembler) {
+		assemblerMap.put(c, assembler);
+
+	}
+
+	public static <T> ResultAssembler<T> getConverter(Class<T> c) {
+		return (ResultAssembler<T>) assemblerMap.get(c);
+	}
+
+}
