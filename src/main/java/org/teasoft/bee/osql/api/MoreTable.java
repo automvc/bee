a1bf71b96131f6fa6f1cd06017f8b387afdc927e
Kingstar <honeysoft@126.com>
2024-06-10 23:58:44 +0800

    add public <T> List<String[]> selectString(T entity, Condition condition) in MoreTable

--- src/main/java/org/teasoft/bee/osql/api/MoreTable.java
+++ src/main/java/org/teasoft/bee/osql/api/MoreTable.java
@@ -237,4 +237,24 @@ public interface MoreTable extends CommOperate {
 	 */
 	public <T> int count(T entity, Condition condition);
 	
+	/**
+	 * Instead of returning data in a entity structure, it uses a character array in List.
+	 * @param entity table's entity(do not allow null).
+	 * @param condition Condition as filter the record.
+	 * @return list can contain more than one record with String array struct.
+	 * @since 2.4.0
+	 */
+	 public <T> List<String[]> selectString(T entity, Condition condition);
+	
+	 
+		/**
+		 * Select and return data in Json format according to entity object.
+		 * @param entity table's entity(do not allow null).
+		 * @param condition Condition as filter the record.
+		 * @return Json string, it transform from list which can contain more than one entity.
+		 */
+//		public <T> String selectJson(T entity, Condition condition);
+	 //要考虑组装结果时,如何解析
+	 
+	 
 }
