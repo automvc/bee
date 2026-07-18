1e599ee73c7e00c0979b58431a3b6677fdc1d907
Kingstar <honeysoft@126.com>
2026-07-18 12:32:47 +0800

    add OneMethod1

---
+++ src/main/java/org/teasoft/bee/osql/OneMethod1.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2022-2026 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.bee.osql;
+
+/**
+ * @author Kingstar
+ * @since  3.0.0
+ */
+public interface OneMethod1<T> {
+
+	public T doOneMethod(String para1);
+
+}
